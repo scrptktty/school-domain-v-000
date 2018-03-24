d0cb7ab42a8d70ecbf5c88edb4ec5a155df304d4
Kachel Wittig <kjwittig@gmail.com>
2018-03-24 01:45:16 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,10 +1,14 @@
 class School
   attr_reader :roster, :name
 
-  def initialize(name, roster)
+  def initialize(name, )
    @name = name
    @roster = {}
  end
+
+  def roster=(roster)
+    roster = {}
+  end
 
   # def add_student(roster)
   # end
